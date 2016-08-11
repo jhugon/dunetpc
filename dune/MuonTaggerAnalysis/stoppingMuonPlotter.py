5092d44ecd9a673af0c64331c48759a9a5c61aaa
Justin Hugon <jhugon@fnal.fake>
2016-08-11 15:28:04 -0500

    stoppingMuonPlotter: much improved

--- dune/MuonTaggerAnalysis/stoppingMuonPlotter.py
+++ dune/MuonTaggerAnalysis/stoppingMuonPlotter.py
@@ -67,10 +67,20 @@ def plotVariable1D(tree,canvas,variable,nBinsX,xMin,xMax,xlabel,ylabel,saveName,
   name = "hist"+str(random.getrandbits(36))
   tree.Draw(variable+" >> "+name+"("+binningString+")",cuts,drawopt)
   hist = root.gPad.GetPrimitive(name)
+  hist.UseCurrentStyle()
   hist.SetTitle("")
+  #hist.Scale(1./200000)
+  #print(hist.Integral())
+  #ylabel = "Fraction of Sample"
   setHistTitles(hist,xlabel,ylabel)
   drawStandardCaptions(canvas,caption,captionleft1,captionleft2,captionleft3,captionright1,captionright2,captionright3,preliminaryString)
   canvas.SaveAs(saveName)
+  #canvas.SaveAs(saveName+".C")
+  #canvas.SaveAs(saveName+".root")
+  #outfile = root.TFile(saveName+".root","RECREATE")
+  #outfile.cd()
+  #hist.Write()
+  #outfile.Close()
   return hist
 
 def plotVariable2D(tree,canvas,variable,nBinsX,xMin,xMax,nBinsY,yMin,yMax,xlabel,ylabel,saveName,cuts="",drawopt="COLZ",logx=False,logy=False,caption="",captionleft1="",captionleft2="",captionleft3="",captionright1="",captionright2="",captionright3="",preliminaryString=""):
@@ -90,9 +100,11 @@ def plotVariable2D(tree,canvas,variable,nBinsX,xMin,xMax,nBinsY,yMin,yMax,xlabel
   name = "hist"+str(random.getrandbits(36))
   tree.Draw(variable+" >> "+name+"("+binningString+")",cuts,drawopt)
   hist = root.gPad.GetPrimitive(name)
+  hist.UseCurrentStyle()
   hist.SetTitle("")
   setHistTitles(hist,xlabel,ylabel)
   drawStandardCaptions(canvas,caption,captionleft1,captionleft2,captionleft3,captionright1,captionright2,captionright3,preliminaryString)
+  canvas.RedrawAxis()
   canvas.SaveAs(saveName)
   setupCOLZFrame(canvas,reset=True)
   return hist
@@ -103,7 +115,7 @@ if __name__ == "__main__":
   f = root.TFile("MuonTaggerTree.root")
   
   tree = f.Get("muontaggertreemaker/tree")
-  tree.Print()
+  #tree.Print()
   
   #for iEvent in range(tree.GetEntries()):
   #for iEvent in range(100):
@@ -112,19 +124,59 @@ if __name__ == "__main__":
   #    print tree.xe, tree.ye, tree.ze, tree.pe
   #tree.Scan("numberTrajectoryPoints")
 
-  plotVariable1D(tree,c,"xb",100,-1000,1000,"Muon Starting x [cm]","Events/bin","Start_x.png",cuts="")
+  ## I think the sample assumes the detector is x: 0-200 and z: 0-150
+
+  plotVariable1D(tree,c,"xb",100,-100,300,"Muon Starting x [cm]","Events/bin","Start_x.png",cuts="")
   plotVariable1D(tree,c,"yb",500,0,1000,"Muon Starting y [cm]","Events/bin","Start_y.png",cuts="")
-  plotVariable1D(tree,c,"zb",100,-2000,2000,"Muon Starting z [cm]","Events/bin","Start_z.png",cuts="")
+  plotVariable1D(tree,c,"zb",100,-100,250,"Muon Starting z [cm]","Events/bin","Start_z.png",cuts="")
   #plotVariable2D(tree,c,"yb:xb",40,-400,400,40,0,800,"Muon Starting x [cm]","Muon Starting y [cm]","Start_yVx.png",cuts="")
   #plotVariable2D(tree,c,"zb:yb",40,0,800,35,-10,690,"Muon Starting y [cm]","Muon Starting z [cm]","Start_zVy.png",cuts="")
-  plotVariable2D(tree,c,"zb:xb",40,-400,400,35,-10,690,"Muon Starting x [cm]","Muon Starting z [cm]","Start_zVx.png",cuts="")
+  plotVariable2D(tree,c,"zb:xb",40,-100,300,35,-100,250,"Muon Starting x [cm]","Muon Starting z [cm]","Start_zVx.png",cuts="")
+  plotVariable2D(tree,c,"zb:xb",40,-100,300,35,-100,250,"Muon Starting x [cm]","Muon Starting z [cm]","Start_zVx_cuts.png",cuts="thetazenithb > pi*170/180",caption="#theta_{zenith} > 170 deg")
+  plotVariable2D(tree,c,"zb:xb",100,-150,350,100,-150,250,"Muon Starting x [cm]","Muon Starting z [cm]","Start_zVx_cuts2.png",cuts="thetazenithb > pi*178/180",caption="#theta_{zenith} > 178 deg")
 
-  plotVariable2D(tree,c,"ye:xe",40,-400,400,40,0,800,"Stopping Muon x [cm]","Stopping Muon y [cm]","Stop_yVx.png",cuts="inWideTPCe && pe<0.01")
-  plotVariable2D(tree,c,"ze:ye",40,0,800,35,-10,690,"Stopping Muon y [cm]","Stopping Muon z [cm]","Stop_zVy.png",cuts="inWideTPCe && pe<0.01")
-  plotVariable2D(tree,c,"ze:xe",40,-400,400,35,-10,690,"Stopping Muon x [cm]","Stopping Muon z [cm]","Stop_zVx.png",cuts="inWideTPCe && pe<0.01")
+  plotVariable2D(tree,c,"ye:xe",8,0,200,12,0,600,"Stopping Muon x [cm]","Stopping Muon y [cm]","Stop_yVx.png",cuts="inWideTPCe && pe<0.01 && ze > 0 && ze < 150")
+  plotVariable2D(tree,c,"ye:ze",6,0,150,12,0,600,"Stopping Muon z [cm]","Stopping Muon y [cm]","Stop_yVz.png",cuts="inWideTPCe && pe<0.01 && xe > 0 && xe < 200")
+  plotVariable2D(tree,c,"ze:xe",8,0,200,6,0,150,"Stopping Muon x [cm]","Stopping Muon z [cm]","Stop_zVx.png",cuts="inWideTPCe && pe<0.01 && ye > 0 && ye < 600")
 
   plotVariable2D(tree,c,"ze:xe",40,-400,400,35,-10,690,"Stopping Muon x [cm]","Stopping Muon z [cm]","Stop_zVx_cuts.png",cuts="inWideTPCe && pe<0.01 && xb < 200 & xb > 0 && zb < 200 && zb > 0",caption="Muon Starting 0<x<200 cm and 0<z<200cm")
 
   plotVariable2D(tree,c,"180-thetazenithb*180/pi:zb",40,-400,400,45,0,90,"Stopping Muon x [cm]","Muon #theta_{zenith} [deg]","thetazVz.png",cuts="")
 
-  tree.Scan("xb:yb:zb:180-thetazenithb*180/pi")
+  plotVariable1D(tree,c,"ye",12,0,600,"Muon Stopping y [cm]","Events/bin","Stop_y.png",cuts="inWideTPCe && pe<0.01 && xe > 0 && xe < 200 && ze > 0 && ze < 150")
+
+
+
+  #tree.Scan("xb:yb:zb:180-thetazenithb*180/pi")
+
+  f2 = root.TFile("MuonTaggerTree_10k.root")
+  tree2 = f2.Get("muontaggertreemaker/tree")
+  trajXY = f2.Get("muontaggertreemaker/trajectoryXY")
+  trajZY = f2.Get("muontaggertreemaker/trajectoryZY")
+  trajXZ = f2.Get("muontaggertreemaker/trajectoryXZ")
+  setupCOLZFrame(c)
+  trajXY.UseCurrentStyle()
+  trajZY.UseCurrentStyle()
+  trajXZ.UseCurrentStyle()
+  trajXY.GetXaxis().SetRangeUser(-200,400)
+  trajXY.GetYaxis().SetRangeUser(-500,1000)
+  trajXZ.GetXaxis().SetRangeUser(-200,400)
+  trajXZ.GetYaxis().SetRangeUser(-200,400)
+  trajZY.GetXaxis().SetRangeUser(-200,400)
+  trajZY.GetYaxis().SetRangeUser(-500,1000)
+  setHistTitles(trajXY,"Trajectory point x [cm]","Trajectory point y [cm]")
+  setHistTitles(trajZY,"Trajectory point z [cm]","Trajectory point y [cm]")
+  setHistTitles(trajXZ,"Trajectory point x [cm]","Trajectory point z [cm]")
+  trajXY.Draw("colz")
+  c.SaveAs("trajXY.png")
+  trajZY.Draw("colz")
+  c.SaveAs("trajZY.png")
+  trajXZ.Draw("colz")
+  c.SaveAs("trajXZ.png")
+
+#  #plotVariable2D(tree2,c,"trajy:trajx",100,-200,400,100,-500,1000,"Trajectory point x [cm]","Trajectory point y [cm]","trajXY_tree.png",cuts="")
+#  plotVariable2D(tree2,c,"trajy:trajx",100,-200,400,100,-500,1000,"Trajectory point x [cm]","Trajectory point y [cm]","trajXY_cuts1.png",cuts="thetazenithb < pi*150/180",caption="#theta_{zenith} < 150 deg")
+#  plotVariable2D(tree2,c,"trajy:trajx",100,-200,400,100,-500,1000,"Trajectory point x [cm]","Trajectory point y [cm]","trajXY_cuts2.png",cuts="xb < 200 & xb > 0 && zb < 200 && zb > 0",caption="Muon Starting 0<x<200 cm and 0<z<200cm")
+#  plotVariable2D(tree2,c,"trajy:trajx",100,-200,400,100,-500,1000,"Trajectory point x [cm]","Trajectory point y [cm]","trajXY_cuts3.png",cuts="!(xb < 200 & xb > 0 && zb < 200 && zb > 0)",caption="Muon Not Starting in 0<x<200 cm and 0<z<200cm")
+#  plotVariable2D(tree2,c,"trajy:trajx",100,-200,400,100,-500,1000,"Trajectory point x [cm]","Trajectory point y [cm]","trajXY_cuts4.png",cuts="inWideTPCe && pe<0.01 ",caption="Stopping Muons in TPC")
+
